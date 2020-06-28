4efc4cde835562e599f8e72df573b2fc59a6b5d5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-06-28 15:03:23 -0700

    numpy, SymPy/caracas and data.table on all images

--- inst/docker-context/Installers/python-interface.R
+++ inst/docker-context/Installers/python-interface.R
@@ -2,6 +2,7 @@
 
 to_install <- c(
   "caracas",
+  "data.table",
   "keras",
   "reticulate",
   "rTorch"
@@ -9,9 +10,19 @@ to_install <- c(
 install.packages(to_install, quiet = TRUE)
 warnings()
 
+print("")
 print("Missing packages?")
 print(setdiff(to_install, as.data.frame(installed.packages())$Package))
 
 reticulate::py_discover_config()
+print("")
 print("Is keras available?")
 keras::is_keras_available()
+print("")
+print("Installed R packages")
+print(
+  subset(
+    as.data.frame(
+      installed.packages()),
+    select = c(Package, Version)),
+row.names = FALSE, max = 1000000)
