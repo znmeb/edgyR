9b50b18813550c1bb9031341516cb3017166641b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-06-30 15:50:46 -0700

    Revert "TinyTeX removed - errors are annoying"
    This reverts commit 28ee9a302fb846dbdaad7e64c095331fbed37bf2.

---
+++ inst/docker-context/Installers/tinytex.R
@@ -0,0 +1,13 @@
+#! /usr/bin/env Rscript
+
+to_install <- c(
+  "tinytex"
+)
+install.packages(to_install, quiet = TRUE)
+warnings()
+
+print("Missing packages?")
+print(setdiff(to_install, as.data.frame(installed.packages())$Package))
+
+print("You can ignore errors in the following TinyTeX install")
+try(tinytex::install_tinytex())
