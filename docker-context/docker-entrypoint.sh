bb72c00f46fa1db0d2e059530344ebbd31dea09e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-07-04 18:35:33 -0700

    add a note that the server is starting

--- docker-context/docker-entrypoint.sh
+++ docker-context/docker-entrypoint.sh
@@ -6,6 +6,7 @@ if [ ${#EDGYR_PASSWORD} -ge 12 ]
 then
   echo "Resetting 'edgyr' password"
   echo "edgyr:${EDGYR_PASSWORD}" | chpasswd
+  echo "Starting RStudio Server - browse to port 8787 on Docker host"
 else
   echo "New 'edgyr' password must be at least 12 bytes!"
   echo "Exiting!"
