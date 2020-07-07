210d24683e3ea9b43ca4313936345aa996ec6107
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-07-07 15:44:29 -0700

    simplify build scripts

--- docker-build-context/build-image.bash
+++ docker-build-context/build-image.bash
@@ -9,33 +9,6 @@ then
   exit -10
 fi
 
-# check the base image
-if [ "$1" == "" ]
-then
-  echo "You need to specify a base image as the first argument,"
-  echo "an output image tag as the second argument,"
-  echo "and a Dockerfile as the third argument."
-  exit -1
-fi
-
-# check the output image
-if [ "$2" == "" ]
-then
-  echo "You need to specify a base image as the first argument,"
-  echo "an output image tag as the second argument,"
-  echo "and a Dockerfile as the third argument."
-  exit -2
-fi
-
-# check the Dockerfile
-if [ "$3" == "" ]
-then
-  echo "You need to specify a base image as the first argument,"
-  echo "an output image tag as the second argument,"
-  echo "and a Dockerfile as the third argument."
-  exit -3
-fi
-
 # check the repository definition
 if [ "$DOCKER_REPO" == "" ]
 then
@@ -44,16 +17,27 @@ then
   exit -20
 fi
 
+# check the output image
+if [ "$1" == "" ]
+then
+  echo "You need to specify an output image tag as the first argument,"
+  echo "and a Dockerfile as the second argument."
+  exit -1
+fi
+
+# check the Dockerfile
+if [ "$2" == "" ]
+then
+  echo "You need to specify an output image tag as the first argument,"
+  echo "and a Dockerfile as the second argument."
+  exit -2
+fi
+
 echo "Building the image"
 /usr/bin/time sudo docker build \
-  --build-arg BASE_IMAGE="$1" \
-  --build-arg R_SOURCE_TARBALL="https://cloud.r-project.org/src/base/R-4/R-4.0.2.tar.gz" \
-  --build-arg RSTUDIO_VERSION_MAJOR=1 \
-  --build-arg RSTUDIO_VERSION_MINOR=3 \
-  --build-arg RSTUDIO_VERSION_PATCH=959 \
-  --tag="$2" \
-  --file="$3" \
+  --tag="$1" \
+  --file="$2" \
 .
 
-sudo docker tag "$2" "$DOCKER_REPO/$2"
+sudo docker tag "$1" "$DOCKER_REPO/$1"
 sudo docker images
