0edb45259139bd3a15a35b909af7cd812dca87b5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-07-19 23:33:53 -0700

    next refactoring round for smoke test

--- docker-build-context/Installers/devtools.R
+++ docker-build-context/Installers/devtools.R
@@ -4,7 +4,9 @@ to_install <- c(
   "caracas",
   "data.table",
   "devtools",
+  "DiagrammeR",
   "greta",
+  "igraph",
   "keras",
   "knitr",
   "miniUI",
@@ -15,20 +17,24 @@ to_install <- c(
 )
 
 # update old packages into user library
+print("")
+print("Updating system packages")
 install.packages(as.data.frame(old.packages())$Package)
 
 # do TinyTeX first - it's sensitive to network issues
+print("")
+print("Installing TinyTeX")
+print("You can ignore errors in the following TinyTeX install")
 install.packages("tinytex", quiet = TRUE)
 warnings()
-print("")
-print("You can ignore errors in the following TinyTeX install")
 try(tinytex::install_tinytex())
 print("You can ignore errors in the preceding TinyTeX install")
 print("Testing TinyTeX - these errors matter!")
 options(warn = 2)
 print(tinytex::tlmgr_conf())
-print("")
 
+print("")
+print("Installing developer tools")
 install.packages(to_install, quiet = TRUE)
 warnings()
 
