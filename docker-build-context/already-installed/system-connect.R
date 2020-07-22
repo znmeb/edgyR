77c4bb090e5bfcb659754fef28b72a90dc403925
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-07-21 20:21:57 -0700

    test after install

---
+++ docker-build-context/already-installed/system-connect.R
@@ -0,0 +1,36 @@
+#! /usr/bin/env Rscript
+
+# update old packages into user library
+print("")
+print("Updating system packages")
+install.packages(as.data.frame(old.packages())$Package)
+
+print("")
+print("Installing new packages")
+install.packages(c(
+  "caracas",
+  "data.table",
+  "IRkernel",
+  "keras",
+  "rTorch"
+), quiet = TRUE)
+warnings()
+IRkernel::installspec()
+
+print("")
+print("Initializing Python 3")
+reticulate::use_python("/usr/bin/python3", required = TRUE)
+print(reticulate::py_config())
+print("")
+print("Is 'keras' available?")
+print(keras::is_keras_available())
+print("")
+print("Does 'caracas' work?")
+library(caracas)
+A <- matrix(c("x", 2, 0, "2*x"), 2, 2)
+B <- as_symbol(A)
+print(B)
+Binv <- inv(B)
+print(Binv)
+tex(Binv)
+print(eigen_val(Binv))
